aa5bf5a75bca894ec53506e93c6e05ec7ed4b475
jyuni125 <jyuni12tafy@gmail.com>
2024-01-31 19:16:18 +0800

    Token Service and JWT config and CreateToken

---
+++ Core/CleanArchitecture.Domain/Contracts/ITokens/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Contracts.ITokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal? GetPrincipalFromExpiredToken();
+    }
+}
